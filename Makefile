e8ba59979da108d964b28e20759db3c3dd0e456a
unknown <01169577@pw.edu.pl>
2022-03-27 17:33:06 +0200

    pierwsza wersja kolejki priorytetowej pod kontem algorytmu dijkstry

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ CC=gcc
 
 readtest: mainreadtest.c files.c list.c
 	$(CC) -o readtest mainreadtest.c files.c list.c
+pqtest: pqmain.c pq.c
+	$(CC) -o pqtest pqmain.c pq.c
---
+++ pq.h
@@ -0,0 +1,31 @@
+#ifndef PQ_H
+#define PQ_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+#include "files.h"
+
+typedef struct heapNode{
+    double weigth;
+    int val;
+}*hn_t;
+
+typedef struct pq{
+    hn_t *q; //lista
+    int n; //licza węzłow w liście (obecnie)
+    int s; //rozmiar
+}*pq_t;
+
+pq_t init(int s);
+void swap(hn_t *a, hn_t *b);
+void heapify(pq_t pq, int i);
+void add(pq_t pq, int val, double weigth);
+void printpq(pq_t pq);
+void del(pq_t pq, int num);
+int isEmpty(pq_t pq);
+hn_t getMin(pq_t pq);
+void dijkstra(list_t graph, int src, int size);
+
+
+#endif
